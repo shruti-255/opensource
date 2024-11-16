6f2fecc2eb6aa9a6f8a59a69cfc1c406172a8bca
shruti-255 <shrutipatil1325@gmail.com>
2024-11-16 17:41:12 +0530

    Create DND.py

---
+++ visa_prep/DND.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+num1 = num2 = 0
+for i in arr:
+    if(i%m==0):
+        num1 += i
+    else:
+        num2 += i
+print(num1 - num2)
