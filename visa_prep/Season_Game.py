eb90b7e543ae58494f18b5c59669240d5b0c3c46
shruti-255 <shrutipatil1325@gmail.com>
2024-11-17 16:10:27 +0530

    Create Season_Game.py

---
+++ visa_prep/Season_Game.py
@@ -0,0 +1,11 @@
+n = int(input())
+if(3<=n<=5):
+    print("Spring")
+elif(6<=n<=8):
+    print("Summer")
+elif(9<=n<=11):
+    print("Autumn")
+elif(n==12 or 1<=n<=2):
+    print("Winter")
+else:
+    print("Invalid")
